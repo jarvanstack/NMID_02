34eb4baa76f46e1690decf2d9fd342b7383caf30
半亩方糖 <dengjiawen8955@gmail.com>
2020-07-20 16:55:47 +0800

    解决class文件加载问题

--- src/main/java/com/sims/dao/BaseDao.java
+++ src/main/java/com/sims/dao/BaseDao.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -13,33 +15,50 @@ import java.util.Properties;
  * 基础Dao
  */
 public class BaseDao {
-    //获取connection
-    public static Connection getConnection(){
-        Connection connection = null;
-        DruidDataSource druidDataSource = new DruidDataSource();
+    static DruidDataSource druidDataSource = null;
+    private static void init() throws IOException {
+        druidDataSource = new DruidDataSource();
         InputStream is = BaseDao.class.getClassLoader().getResourceAsStream("db.properties");
         Properties properties = new Properties();
+        assert is != null;//
+        // 种加载是加载的 classes 文件下的 resources 会自动到 classes文件下，如果没有及时更新，就跑不起来
+        properties.load(is);
+        String driver = properties.getProperty("driver");
+        String url = properties.getProperty("url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String jdbcPoolInitSize = properties.getProperty("jdbcPoolInitSize");
         druidDataSource.setDriverClassName(properties.getProperty("driver"));
         druidDataSource.setUrl(properties.getProperty("url"));
         druidDataSource.setUsername(properties.getProperty("user"));
-        druidDataSource.setInitialSize(Integer.parseInt(properties.getProperty("jdbcPoolInitSize")));//初始化个数
-        try {
-            connection = druidDataSource.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            properties.load(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-            //异常
-        }
+        druidDataSource.setPassword(properties.getProperty("password"));
+        druidDataSource.setInitialSize(Integer.parseInt(properties.getProperty("jdbcPoolInitSize")));
+    }
 
-        return  connection;
+    //获取connection
+    public static Connection getConnection() throws IOException, SQLException {
+        if (druidDataSource==null){
+            init();//如果没有初始化就初始化
+        }
+        return  druidDataSource.getConnection();
     }
     @Test  //测试数据池 getConnection()
     public void test(){
-        Connection connection = getConnection();
-
+        Connection connection = null;
+        try {
+            connection = getConnection();
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+        String sql = "SELECT * FROM `sims`.`account` LIMIT 0,1000";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("a_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: 25301
   Date: 2020/7/20
-  Time: 15:08
+  Time: 16:23
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
